68694b149f331c23f2e33b11cc641a1b1efabfda
teeradet025 <teeradet.math@gmail.com>
2025-03-17 09:50:33 +0700

    0.2

--- Assets/Scrip/Ro.cs
+++ Assets/Scrip/Ro.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Ro : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] Vector3 angularV , torque;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            rb.angularVelocity = angularV;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        { 
+         rb.AddTorque(torque);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.angularVelocity = Vector3.zero ;
+        }
+
     }
 }
